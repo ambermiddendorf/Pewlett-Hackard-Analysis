1db027f7cc64014ddb23c82ef17d3e6b43330080
ambermiddendorf <ambermiddendorf@hotmail.com>
2022-02-07 21:41:09 -0600

    created retirement_info table

--- schema.sql
+++ schema.sql
@@ -50,6 +50,6 @@ CREATE TABLE titles (
 	title VARCHAR NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE,
-FOREIGN KEY (emp_no) REFERENCES employees (emp_no),	
-	PRIMARY KEY (emp_no)
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
+
